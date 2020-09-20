3ee29a773d83bd3cd932df072a93a97613a0b494
OscarDHdz <oscardavid.hernandez.mx@gmail.com>
2020-09-20 16:04:45 -0500

    Move config into a configuration file

--- src/components/common/ConfigContext.js
+++ src/components/common/ConfigContext.js
@@ -1,25 +1,11 @@
 import React, { createContext, useState } from 'react';
+import envConfig from '../../config/config.json';
 
-const ConfigContext = createContext({
-  baseUrl: ''
-});
+const ConfigContext = createContext(envConfig);
 
 export const ConfigContextProvider = (props) => {
-
-  const [config] = useState({
-    baseUrl: 'http://localhost:3000',
-    systemTypes: [
-      { value: 'WINDOWS_WORKSTATION', display: 'Windows Workstation' },
-      { value: 'MAC', display: 'Mac' },
-      { value: 'WINDOWS_SERVER', display: 'Windows Server' }
-    ],
-    sortOptions: [
-      { value: '', display: '-' },
-      { value: 'hdd_capacity', display: 'HDD Capacity' },
-      { value: 'system_name', display: 'System Name' }
-    ]
-  });
-
+  // Hooks
+  const [config] = useState(envConfig);
 
   return (
     <ConfigContext.Provider value={{
