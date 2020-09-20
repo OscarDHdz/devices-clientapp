6f5e20320f7c4da54ed587c9a09cd8222e861497
OscarDHdz <oscardavid.hernandez.mx@gmail.com>
2020-09-20 15:23:00 -0500

    Refactor Device List Toolbar - Move to a new component

--- src/components/devices/DevicesList.js
+++ src/components/devices/DevicesList.js
@@ -1,21 +1,23 @@
 import React, { Fragment, useContext, useState } from 'react';
 import { sortObjectsFn } from '../../utils/sort';
 import Alert from '../common/Alert';
-import Badge from '../common/Badge';
 import Device from './Device';
 import DeviceContext from './DeviceContext';
 import './DeviceList.css';
 import DeviceModal from './DeviceModal';
+import DeviceListToolbar from './DevicesListToolbar';
 
 const DevicesList = ({devices = []}) => {
   const deviceContext = useContext(DeviceContext);
 
   // Hooks
-  const [typeFilter, setTypeFilter] = useState([]);
-  const [sortBy, setSortBy] = useState('');
-  const [sortDirection] = useState('asc');
   const [openEditModal, setOpenEditModal] = useState(false);
   const [deviceToEdit, setDeviceToEdit] = useState(null);
+  const [viewCriteria, setViewCriteria] = useState({
+    typeFilter: [],
+    sortBy: '',
+    sortDirection: 'asc'
+  });
 
   // Device Component Handlers
   const handleDeviceDelete = async (deviceToDelete) => {
@@ -42,22 +44,9 @@ const DevicesList = ({devices = []}) => {
     setDeviceToEdit(null);
   }
 
-  /**
-   * Handle filter field - Append badges/filter
-   * @param {event} event 
-   */
-  const handleFilterChange = (event) => {
-    const selectedVal = event.target.value;
-    setTypeFilter([...typeFilter, selectedVal]);
-  }
-  /**
-   * Handle filter field - remove badge/filter
-   * @param {string} badgeValue 
-   */
-  const handleBadgeRemove = (badgeValue) => {
-    const idx = typeFilter.findIndex(filter => filter === badgeValue);
-    typeFilter.splice(idx, 1);
-    setTypeFilter([...typeFilter]);
+  // Handler for toolbar filter/sort/direction
+  const handleCriteriaChange = (criteria) => {
+    setViewCriteria(criteria);
   }
 
 
@@ -68,30 +57,16 @@ const DevicesList = ({devices = []}) => {
   const getVisibleDevices = () => {
     // Apply Sort 
     let devicesToDisplay = [...devices];
-    if (sortBy) {
-      devicesToDisplay.sort((a, b) => sortObjectsFn(a, b, sortBy, sortDirection));
+    if (viewCriteria.sortBy) {
+      devicesToDisplay.sort((a, b) => sortObjectsFn(a, b, viewCriteria.sortBy, viewCriteria.sortDirection));
     }
     // Apply Filter
-    if (typeFilter.length > 0) {
+    if (viewCriteria.typeFilter.length > 0) {
       devicesToDisplay = devicesToDisplay.filter(device => {
-        return typeFilter.indexOf(device.type) !== -1;
+        return viewCriteria.typeFilter.indexOf(device.type) !== -1;
       });
     }
     return devicesToDisplay;
-  }
-
-  /**
-   * This function return the 'inactive' filters
-   */
-  const getAvailableFilterOpts = () => {
-    const availableFilters = [];
-    for (const opt of deviceContext.systemTypes) {
-      if ( typeFilter.indexOf(opt.value)  === -1 ) {
-        availableFilters.push(opt);
-      }
-    }
-    availableFilters.sort((a, b) => sortObjectsFn(a, b, 'value', 'asc'));
-    return availableFilters;
   }
 
   const visibleDevices = getVisibleDevices();
@@ -105,43 +80,14 @@ const DevicesList = ({devices = []}) => {
         afterSubmit={handleAfterSubmit}
       ></DeviceModal>
 
-      <div className="deviceListToolbar">
-        <div className="deviceListFilterField">
-          <label>Filter Device Type:</label>
-          <select 
-            value={''}
-            onChange={handleFilterChange}
-          >
-            <option key="" value=""></option>
-            {
-              getAvailableFilterOpts().map(opt => (<option key={opt.value} value={opt.value}>{opt.display}</option>))
-            }
-          </select>
-        </div>
-        <div className="deviceListSortByField">
 
-          <label>Sort By:</label>
-          <select value={sortBy}
-            onChange={event => setSortBy(event.target.value)}
-          >
-            {
-              deviceContext.sortOptions.map(opt => (<option key={opt.value} value={opt.value}>{opt.display}</option>))
-            }
-          </select>
+      <DeviceListToolbar
+        onCrieriaChange={handleCriteriaChange}
+      >
 
-        </div>
-      </div>
-      <div className="deviceListToolbar">
-        {
-          typeFilter.length > 0 ?
-          typeFilter.map(filter => (
-            <Badge key={filter} onClick={() => handleBadgeRemove(filter)}>
-              {deviceContext.systemTypes.find(item => item.value === filter).display}
-            </Badge>
-          ))
-          : ''
-        }
-      </div>
+      </DeviceListToolbar>
+
+
       {
         visibleDevices.length > 0 ?
           (visibleDevices).map(deviceData => (
--- src/components/devices/DevicesListToolbar.js
+++ src/components/devices/DevicesListToolbar.js
@@ -1,50 +1,104 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { sortObjectsFn } from '../../utils/sort';
+import Badge from '../common/Badge';
 import DeviceContext from './DeviceContext';
 
-const DevicesListToolbar = ({onCriteriaChange}) => {
+const DeviceListToolbar = ({onCrieriaChange}) => {
+  const deviceContext = useContext(DeviceContext);
+
   // Hooks
-  const [typeFilter, setTypeFilter] = useState('');
+  const [typeFilter, setTypeFilter] = useState([]);
   const [sortBy, setSortBy] = useState('');
   const [sortDirection] = useState('asc');
 
+  /**
+   * Effect to propagate criteria change to parent
+   */
   useEffect(() => {
-    onCriteriaChange({
-      typeFilter,
-      sortBy,
-      sortDirection
-    });
-  }, [typeFilter, sortBy, sortDirection, onCriteriaChange]);
+    onCrieriaChange({
+      typeFilter, sortBy, sortDirection
+    })
+  }, [typeFilter, sortBy, sortDirection])
 
-  const deviceContext = useContext(DeviceContext);
+  
+  /**
+   * Handle filter field - Append badges/filter
+   * @param {event} event 
+   */
+  const handleFilterChange = (event) => {
+    const selectedVal = event.target.value;
+    setTypeFilter([...typeFilter, selectedVal]);
+  }
+  /**
+   * Handle filter field - remove badge/filter
+   * @param {string} badgeValue 
+   */
+  const handleBadgeRemove = (badgeValue) => {
+    const idx = typeFilter.findIndex(filter => filter === badgeValue);
+    typeFilter.splice(idx, 1);
+    setTypeFilter([...typeFilter]);
+  }
+
+  
+  /**
+   * This function return the 'inactive' filters
+   */
+  const getAvailableFilterOpts = () => {
+    const availableFilters = [];
+    for (const opt of deviceContext.systemTypes) {
+      if ( typeFilter.indexOf(opt.value)  === -1 ) {
+        availableFilters.push(opt);
+      }
+    }
+    availableFilters.sort((a, b) => sortObjectsFn(a, b, 'value', 'asc'));
+    return availableFilters;
+  }
+
+
 
   return (
+          
+
     <div className="deviceListToolbar">
-      <div className="deviceListFilterField">
-        <label>Device Type:</label>
-        <select 
-          value={typeFilter}
-          onChange={event => setTypeFilter(event.target.value)}
-        >
-          <option key="" value="">All</option>
-          {
-            deviceContext.systemTypes.map(opt => (<option key={opt.value} value={opt.value}>{opt.display}</option>))
-          }
-        </select>
-      </div>
-      <div className="deviceListSortByField">
-
-        <label>Sort By:</label>
-        <select value={sortBy}
-          onChange={event => setSortBy(event.target.value)}
-        >
-          {
-            deviceContext.sortOptions.map(opt => (<option key={opt.value} value={opt.value}>{opt.display}</option>))
-          }
-        </select>
-
-      </div>
+    <div className="deviceListFilterField">
+      <label>Filter Device Type:</label>
+      <select 
+        value={''}
+        onChange={handleFilterChange}
+      >
+        <option key="" value=""></option>
+        {
+          getAvailableFilterOpts().map(opt => (<option key={opt.value} value={opt.value}>{opt.display}</option>))
+        }
+      </select>
     </div>
+    <div className="deviceListSortByField">
+
+      <label>Sort By:</label>
+      <select value={sortBy}
+        onChange={event => setSortBy(event.target.value)}
+      >
+        {
+          deviceContext.sortOptions.map(opt => (<option key={opt.value} value={opt.value}>{opt.display}</option>))
+        }
+      </select>
+
+    </div>
+    
+    {
+      typeFilter.length > 0 ?
+      typeFilter.map(filter => (
+        <Badge key={filter} onClick={() => handleBadgeRemove(filter)}>
+          {deviceContext.systemTypes.find(item => item.value === filter).display}
+        </Badge>
+      ))
+      : ''
+    }
+  
+  </div>
+  
   )
 }
 
-export default  DevicesListToolbar;
+
+export default DeviceListToolbar;
